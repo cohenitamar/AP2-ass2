7b1be2816018bbb51dd4adf274ebb9ff5076afcb
Itamar Cohen <itamar248@gmail.com>
2023-05-28 22:55:18 +0300

    Changes by Ori Arad

--- src/ChatComponent/Profile/Profile.js
+++ src/ChatComponent/Profile/Profile.js
@@ -1,7 +1,33 @@
 import accountsDatabase from "../../LoginComponent/AccountsDatabase";
 import {useState} from "react";
 
-function Profile({username}) {
+function Profile({username, token}) {
+    const [user, setUser] = useState({
+        "username": "",
+        "displayName": "",
+        "profilePic": ""
+    });
+    /// get user by username
+/*    {
+        "username": "string",
+        "displayName": "string",
+        "profilePic": "string"
+    }*/
+
+
+    const foo = async () => {
+        const res = await fetch(`http://localhost:5000/api/Users/${username}`, {
+            'method': 'get',
+            'headers': {
+                'Content-Type': 'application/json',
+                'Authorization': "Bearer " + token
+            }
+        })
+        setUser(await res.json());
+    }
+
+
+    foo();
     return (
         <span className="text-truncate">
              <img className="profile-image me-2 " src={user["profilePic"]} alt=""/>

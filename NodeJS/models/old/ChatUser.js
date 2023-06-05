4069c5fc89108766fafc4cca2ffefc8a1b0e1e8f
Itamar Cohen <itamar248@gmail.com>
2023-06-05 23:53:58 +0300

    deleting a folder

--- NodeJS/models/old/ChatUser.js
+++
@@ -1,9 +0,0 @@
-const mongoose = require("mongoose");
-
-const chatUserSchema = new mongoose.Schema({
-    ChatsId: { type: mongoose.Schema.Types.ObjectId, required: true },
-    me: { type: String, required: true },
-    talkingTo: { type: String, required: true },
-});
-
-module.exports = mongoose.model('ChatUser', chatUserSchema);
